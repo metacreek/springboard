e6761ac10bf64629b668b211da03308389f19a26
Harold Neal <harold@hlneal.com>
2019-12-11 18:36:06 -0500

    made a more flexible way to set up the config file

--- data-collection/commands/start.py
+++ data-collection/commands/start.py
@@ -7,5 +7,5 @@ import time
 while ts.count_crawlers() > 0:
     ts.kill_crawlers()
     time.sleep(5)
-ts.create_config_file(before=10000)
+ts.create_config_file(before=10000, user_agent='googlebot', sitelist='sitelist_alt')
 ts.start_crawlers()
--- data-collection/commands/topicsentiment.py
+++ data-collection/commands/topicsentiment.py
@@ -9,13 +9,20 @@ def start_date(before=0):
     return str(start)
 
 
-def create_config_file(before=10):
+def create_config_file(before=10, user_agent='default', sitelist='sitelist'):
     with open(f'{BASEDIR}/config/config_base.cfg') as infile, open(f'{BASEDIR}/config/config.cfg', 'w') as outfile:
         for line in infile:
             if line.startswith('start_date'):
                 line = f"start_date = '{start_date(before)} 00:00:00'\n"
             elif line.startswith('LOG_FILE'):
-                line = f"LOG_FILE = '{BASEDIR}/log_{start_date()}.txt'\n"
+                line = f"LOG_FILE = '{BASEDIR}/log_{start_date()}_{user_agent}_{sitelist}_{before}.txt'\n"
+            elif line.startswith('USER_AGENT'):
+                if user_agent == 'default':
+                    line = f"USER_AGENT = 'news-please (+http://www.example.com/)'"
+                elif user_agent == 'googlebot':
+                    line = f"USER_AGENT = 'Googlebot'"
+            elif line.startswith('url_input_file_name'):
+                line = f"url_input_file_name = {sitelist}.hjson"
             outfile.write(line)
 
 
