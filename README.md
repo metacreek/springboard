5283abf38ceafac8afb4aa24ba42df5a6e8a3213
Harold Neal <harold@hlneal.com>
2020-04-23 14:14:27 -0400

    added a more info to root README

--- README.md
+++ README.md
@@ -5,5 +5,12 @@ project involved the full lifecycle of data collection, analysis, wrangling, and
 The project adapts a Bert-based model to be used for classification of text from 
 dozens of publicans.  It was built using both AWS and Google Cloud.
 
+* [data-collection](https://github.com/metacreek/springboard/tree/master/data-collection) contains code used to crawl dozens of news and opinion websites.
 
+* [data-wrangling](https://github.com/metacreek/springboard/tree/master/data-wrangling) contains Jupyter notebooks outlining evaluation of the collected data and analysis use to wrangle the data into a usable format.
 
+* [modeling](https://github.com/metacreek/springboard/tree/master/modeling) contains code and notebooks used to fine tune a BERT model to classify news stories.
+
+* [api](https://github.com/metacreek/springboard/tree/master/api) contains code used to host an API based on my model.
+
+* [mini-projects](https://github.com/metacreek/springboard/tree/master/mini-projects) contains Jupyter notebooks submitted as part of classwork for the Springboard Machine Learning Engineer Bootcamp.  This code is not directly related to the capstone project.
--- modeling/code/clean_and_tokenize.py
+++ modeling/code/clean_and_tokenize.py
@@ -1,4 +1,4 @@
-from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 from datetime import datetime
@@ -10,19 +10,23 @@ import bert
 import pandas as pd
 from google.cloud import storage
 
+# Parts of this code was adapted from https://analyticsindiamag.com/bert-classifier-with-tensorflow-2-0/
+
 spark = (SparkSession.builder
         .config("spark.debug.maxToStringFields", 100)
-         .getOrCreate()
+        .getOrCreate()
          )
 
 sc = spark.sparkContext
-sqlContext = SQLContext(sc)
 
-INPUT_NAME =  'gs://topic-sentiment-1/decade-data/full'
+INPUT_NAME = 'gs://topic-sentiment-1/decade-data/full'
 OUTPUT_NAME = 'tokenized'
 
 
 def log_time(msg):
+    """
+    A short function to measure execution time of various steps
+    """
     print(f"@@@@ {msg} {datetime.now()}")
 
 
@@ -31,6 +35,17 @@ log_time("Begin processing")
 MAX_SEQ_LEN = 256
 MAX_SEQ_LEN_BC = sc.broadcast(MAX_SEQ_LEN)
 
+
+# This project seeks to classify the origin of news stories.  Some publishers
+# include the name of the publication in the crawled stories.  I felt this was a
+# "cheat" that made it too easy to classify stories, so I removed the name
+# from the story body.  The SUBSTITUTION dictionary lists these names to be
+# removed in later code.  In addition, there were certain words that were found
+# to be extremely frequent for certain publications.  This was found during
+# NER (named-entity recognition) which was performed as part of an approach
+# to classifying stories.  That approach was eventually abandoned but still
+# played a useful part in analysis.  It can be seen on the other-approaches
+# code branch.   Additionally, the site host name is removed from the story text.
 SUBSTITUTION = {'ajc.com': ['Atlanta Journal-Constitution', 'Subscribe'],
                 'americanthinker.com': ['Full Post'],
                 'apnews.com': ['AP', 'Associated Press', 'Business Wire', 'Copyright', 'ResearchAndMarkets.com'],
@@ -79,13 +94,20 @@ SUBSTITUTION = {'ajc.com': ['Atlanta Journal-Constitution', 'Subscribe'],
                 'washingtonpost.com': ['Washington Post'],
                 'wsj.com': ['Wall Street Journal', 'Copyright']}
 
+# make a regex pattern for removal
 for key in SUBSTITUTION:
     SUBSTITUTION[key] = "(" + "|".join(SUBSTITUTION[key]) + "|" + key + "|\n)"
 
+# broadcast data to workers
 SUBSTITUTION_BC = sc.broadcast(SUBSTITUTION)
 
 
 def add_regex(source):
+    """
+    Returns the regex string to remove for a particular website.
+
+    :param source: string, canonical website domain name
+    """
     return SUBSTITUTION_BC.value[source]
 
 
@@ -93,6 +115,14 @@ udf_add_regex = F.udf(add_regex)
 
 
 def clean_text(arr):
+    """
+    Removes unwanted text from news stories.
+
+    :param arr: array of strings. The first element is the text to be processed.  The second element is the regex pattern to apply to the text.
+
+    Using an array for input, rather than two separate arguments is a trick that is used to make it easier to create a UDF (user-defined function) that can be
+    applied in parallel across workers.
+    """
     text = arr[0]
     pattern = arr[1]
     return re.sub(pattern, ' ', text)
@@ -102,25 +132,39 @@ udf_clean_text = F.udf(clean_text)
 
 
 def get_tokens(text):
-    stokens = tokenizer.tokenize(text)
-    stop_words = stop_words_bc.value
-    stokens = [token for token in stokens if not token in stop_words]
+    """
+    Returns correctly formatted tokens for given text.  This also removes stopwords.
+
+    :param text: string, body of news story
+    """
+    tokens = tokenizer.tokenize(text)
+    stop_words_val = stop_words_bc.value
+    tokens = [token for token in tokens if token not in stop_words_val]
     token_length = MAX_SEQ_LEN_BC.value - 2
-    stokens = ["[CLS]"] + stokens[:token_length] + ["[SEP]"]
-    return stokens
+    tokens = ["[CLS]"] + tokens[:token_length] + ["[SEP]"]
+    return tokens
 
 
 udf_get_tokens = F.udf(get_tokens)
 
 
 def get_masks(tokens):
+    """
+    Returns a list of masks for the tokens.  An individual mask is 1 if the token is text and 0 if it is empty
+
+    :param tokens: list of tokens
+    """
     max_seq_length = MAX_SEQ_LEN_BC.value
     masks = [1] * len(tokens) + [0] * (max_seq_length - len(tokens))
     return masks
 
 
 def get_segments(tokens):
-    """Segments: 0 for the first sequence, 1 for the second"""
+    """
+    Returns a list of segments, where an individual segment is 0 for the first sequence, and one for the second, if present
+
+    :param tokens: list of tokens
+    """
     max_seq_length = MAX_SEQ_LEN_BC.value
     segments = []
     current_segment_id = 0
@@ -133,7 +177,11 @@ def get_segments(tokens):
 
 
 def get_ids(tokens):
-    """Token ids from Tokenizer vocab"""
+    """
+    Returns a list of integer word ids for use with Bert model
+
+    :param tokens: list of tokens
+    """
     max_seq_length = MAX_SEQ_LEN_BC.value
     token_ids = tokenizer.convert_tokens_to_ids(tokens, )
     input_ids = token_ids + [0] * (max_seq_length - len(token_ids))
@@ -146,6 +194,11 @@ udf_get_ids = F.udf(get_ids, T.ArrayType(T.IntegerType()))
 
 
 def get_source_domains(sdf):
+    """
+    Returns a dict with canonical domain names as keys and unique integer identifiers for the sorted domain namess
+
+    :param sdf: spark dataframe that contains at least one instance of all domain names
+    """
     log_time("Begin source domains")
     source_domains = sdf.select(F.collect_list('source_domain')).first()[0]
     source_domains = set(source_domains)
@@ -156,10 +209,15 @@ def get_source_domains(sdf):
     for val in source_domains:
         domains[val] = i
         i = i + 1
-
     return domains
 
+
 def source_index(source):
+    """
+    Returns id corresponding to a canonical domain name
+
+    :param source: string, canonical domain name
+    """
     return domains_bc.value[source]
 
 
@@ -167,8 +225,16 @@ udf_source_index = F.udf(source_index)
 
 
 def process_data(raw_data_sdf, bert_layer):
+    """
+    Performs the bulk of the work of tokenization and other cleanups. Returns a reduced spark data frame including ids, masks, and segments, and other helpful elements
+
+    :param raw_data_sdf: spark dataframe, the news stories to be processed
+    :param bert_layer: tensorflow Keras layer for the BERT model being used.
+    """
     global stop_words_bc, tokenizer, domains_bc
-    # remove articles with a particular repeated statement
+    # remove articles with a particular repeated statement.  One site (thenation.com) had a particlar habit
+    # of including long sales blurbs in the story text.  Because there were multiple versions of this, I decided
+    # to not use any such news story, because it would give the classifier an unfair signal of the source domain.
     clean_data_sdf = raw_data_sdf.filter(~raw_data_sdf.text_or_desc.contains("Sign up for Take Action"))
     # add weeks column
     clean_data_sdf = clean_data_sdf.withColumn('published_date', F.to_date(F.col('published')))
@@ -177,11 +243,11 @@ def process_data(raw_data_sdf, bert_layer):
     log_time("Begin regex")
 
     clean_data_sdf = clean_data_sdf.withColumn('regex', udf_add_regex('source_domain'))
-
-
+    # remove all the identifying text from stories
     clean_data_sdf = clean_data_sdf.withColumn('clean_text', udf_clean_text(F.array('text_or_desc', 'regex')))
 
     log_time("Begin tokenizer")
+    #load the tokenizer for the BERT model used
     FullTokenizer = bert.bert_tokenization.FullTokenizer
     vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
     do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
@@ -195,6 +261,7 @@ def process_data(raw_data_sdf, bert_layer):
     clean_data_sdf = clean_data_sdf.withColumn('segments', udf_get_segments('tokens'))
     clean_data_sdf = clean_data_sdf.withColumn('ids', udf_get_ids('tokens'))
     clean_data_sdf = clean_data_sdf.withColumn('source_index', udf_source_index('source_domain').cast('int'))
+    # let's slim down the dataframe before we save it to disk.
     clean_data_sdf = clean_data_sdf[['source_domain', 'text_or_desc', 'clean_text', 'published_date', 'year',
                                      'title', 'url', 'weeks', 'tokens', 'masks', 'segments', 'ids', 'source_index']]
 
@@ -202,28 +269,35 @@ def process_data(raw_data_sdf, bert_layer):
     return clean_data_sdf
 
 log_time('Begin stopwords')
+# loads the nltk english stopwords that should be excluded
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 stop_words_bc = sc.broadcast(stop_words)
 
 log_time("Begin Keras layer")
+# if you wanted to use a different BERT model, here is where you would specify it.
+# Because we are fine-tuning the BERT model, we "freeze" the BERT model itself with trainable=False
+# If we did not do this, the number of trainable parameters would skyrocket and training would take
+# a very long time.
 bert_layer = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1", trainable=False)
 
 log_time(("Split data"))
-
+# The spark dataframe is quite large, around 1.5 million stories, so I decided to
+# split it into ten parts.  This allowed me to do analysis with a subset of data and
+# it made it easier to understand how long the saving process was taking.
+# it also saved on memory required for processing.
 raw_data_sdf = spark.read.parquet(INPUT_NAME)
-
 raw_data_sdf.printSchema()
-
 raw_data_split_sdf = raw_data_sdf.randomSplit([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
 
-iteration = 0
 
 gcs = storage.Client()
-
+iteration = 0
 for df in raw_data_split_sdf:
     log_time(f"Begin iteration {iteration}")
     if iteration == 0:
+        # save the domains with their canonical ids.  This only works with large enough data sets
+        # such that it has all domains that will be used.  We only do this the first time through.
         domains = get_source_domains(df)
         domains_bc = sc.broadcast(domains)
         domain_lookup = pd.Series(domains)
