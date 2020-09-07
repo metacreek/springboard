895db798ea004429a69736efc368320e4fbb7fcc
Harold Neal <harold@hlneal.com>
2020-09-07 12:01:49 -0400

    adds first set of api tests and cleans up code

--- api/main.py
+++ api/main.py
@@ -7,24 +7,15 @@ import json
 import tokenizer as tok
 import pandas as pd
 
-
-
-def get_tokenizer():
-    if 'tokenizer' not in globals():
-        global tokenizer
-        tokenizer = tok.FullTokenizer(vocab_file='vocab.txt', do_lower_case=True)
-    return tokenizer
-
-
-def get_stopwords():
-    if 'stopwords' not in globals():
-        global stopwords
-        with open('stopwords', 'r') as f:
-            stopwords = f.read()
-    return stopwords
+MAX_SEQ_LEN = 256  # This must be same value used in wrangling.  This is the number of tokens used in document analysis.
+columns = 5  # Number of columns to use in site dropdown in frontend
 
 
 def lookup():
+    """
+    Returns a dictionary that contains numerical keys for sitenames.  These are
+    loaded from 'domain_lookup.h5', which is created during data wrangling.
+    """
     store = pd.HDFStore('domain_lookup.h5')
     reverse_lookup = store['domain_lookup']
     store.close()
@@ -32,8 +23,44 @@ def lookup():
     lookup = {value: key for (key, value) in dict_items}
     return lookup
 
+
+def sites():
+    """
+    Returns a list of all sites used in model
+    """
+    return list(lookup().values())
+
+
+number_of_sites = len(sites())
+items_per_column = number_of_sites // columns + 1
+
+
+def get_tokenizer():
+    """
+    Returns tokenizer.  This must be the same tokenizer as used during wrangling, so the vocab.txt file
+    and do_lower_case flag must be the same used during wrangling.   I have extracted the Tokenizer class
+    file from TensorFlow so that the code does not need the heavy machinery of TensorFlow to run
+    """
+    if 'tokenizer' not in globals():
+        global tokenizer
+        tokenizer = tok.FullTokenizer(vocab_file='vocab.txt', do_lower_case=True)
+    return tokenizer
+
+
+def get_stopwords():
+    """
+    Returns a string of stopwords which are removed during tokenization.
+    """
+    if 'stopwords' not in globals():
+        global stopwords
+        with open('stopwords', 'r') as f:
+            stopwords = f.read()
+    return stopwords
+
+
 def get_masks(tokens, max_seq_length):
     return [1]*len(tokens) + [0] * (max_seq_length - len(tokens))
+
 
 def get_segments(tokens, max_seq_length):
     """Segments: 0 for the first sequence, 1 for the second"""
@@ -44,6 +71,7 @@ def get_segments(tokens, max_seq_length):
         if token == "[SEP]":
             current_segment_id = 1
     return segments + [0] * (max_seq_length - len(tokens))
+
 
 def get_ids(tokens, max_seq_length):
     """Token ids from Tokenizer vocab"""
@@ -56,12 +84,9 @@ def get_ids(tokens, max_seq_length):
 def create_single_input(sentence, MAX_LEN):
     tokenizer = get_tokenizer()
     stokens = tokenizer.tokenize(sentence)
-    stokens = [token for token in stokens if not token in get_stopwords()]
-
+    stokens = [token for token in stokens if token not in get_stopwords()]
     stokens = stokens[:MAX_LEN]
-
     stokens = ["[CLS]"] + stokens + ["[SEP]"]
-
     ids = get_ids(stokens, MAX_SEQ_LEN)
     masks = get_masks(stokens, MAX_SEQ_LEN)
     segments = get_segments(stokens, MAX_SEQ_LEN)
@@ -69,21 +94,12 @@ def create_single_input(sentence, MAX_LEN):
     return ids, masks, segments
 
 
-def sites():
-    return list(lookup().values())
-
-MAX_SEQ_LEN = 256
-
-columns = 5
-length = len(sites())
-items_per_column = length // columns + 1
-
 def get_next_highest(scores):
-    print(type(scores), scores)
     max = np.max(scores)
     idx = np.argmax(scores)
     scores[idx] = 0
     return max, idx, scores
+
 
 def analyze(request):
     results = []
@@ -95,18 +111,8 @@ def analyze(request):
     elif request_args and 'text' in request_args:
         text = request_args['text']
 
-    print("@@@@@@@", text)
-
-
     if text:
-        print("text", text)
         ids, masks, segments = create_single_input(text, MAX_SEQ_LEN-2)
-        print("ids", ids)
-        print("masks", masks)
-        print("segments", segments)
-        inputs = [np.asarray([ids]), np.asarray([masks]), np.asarray([segments])]
-        print("inputs", inputs)
-
 
         service = discovery.build('ml', 'v1')
         name = 'projects/topic-sentiment-269614/models/springboard_capstone_project'
@@ -117,27 +123,19 @@ def analyze(request):
             "segment_ids": segments}]
         }
 
-        # with open('input.json', 'r') as f:
-        #     body = json.load(f)
-
-        print('body', json.dumps(body))
-
         response = service.projects().predict(
             name=name, body=body
         ).execute()
-        print(response)
 
         if 'error' in response:
             raise RuntimeError(response['error'])
 
         predictions = response['predictions']
-        print("@@@@", predictions)
         predictions = predictions[0]['dense_output']
 
         for i in range(1, 4):
             score, idx, predictions = get_next_highest(predictions)
             results.append((score * 100, idx, lookup()[idx]))
 
-    print("@@@@@", results)
     return render_template('main.html', text=text, sites=sites(), columns=5,
-                           length=length, items=items_per_column, results=results)
+                           length=number_of_sites, items=items_per_column, results=results)
---
+++ api/test_main.py
@@ -0,0 +1,53 @@
+import pytest
+import main
+
+MAX_SEQ_LEN = 256
+
+
+def test_lookup():
+    lookup = main.lookup()
+    assert len(lookup) > 0
+    assert type(lookup) == dict
+    assert type(lookup[0]) == str
+
+
+def test_sites():
+    sites = main.sites()
+    assert len(sites) > 0
+    assert type(sites) == list
+    assert type(sites[0]) == str
+
+
+def test_get_stopwords():
+    sw = main.get_stopwords()
+    assert len(sw) > 0
+    assert type(sw) == str
+    assert "myself" in sw
+
+
+def test_get_masks():
+    masks = main.get_masks("The chalkboard is in the other room", MAX_SEQ_LEN)
+    assert masks == 35 * [1] + 221 * [0]
+
+
+def test_get_segments():
+    segments = main.get_segments("The chalkboard is in the other room", MAX_SEQ_LEN)
+    assert segments == 256 * [0]
+
+
+def test_create_single_input():
+    text = "For me myself, I would not look at it in that way."
+    input = main.create_single_input(text, MAX_SEQ_LEN)
+    assert input == (
+        [101, 1010, 2298, 2126, 1012, 102] + 250 * [0],
+        6 * [1] + 250 * [0],
+        256 * [0]
+    )
+
+
+def test_get_next_highest():
+    predictions = [[0.3, 0.2, 0.1]]
+    next_highest = main.get_next_highest(predictions)
+    print(next_highest)
+    assert next_highest == (0.3, 0, [0])
+
